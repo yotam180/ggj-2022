2b254939ddcae18decb8d2e6bb04e636dbb263d6
shaiTheKimhi <shaikimhi221199@gmail.com>
2022-01-28 18:36:29 +0200

    walls connection

--- Assets/CameraAndGame.cs
+++ Assets/CameraAndGame.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraAndGame : MonoBehaviour
 {
-
+    const float width = 0.4f;
 
     List<List<Vector3>> create_map(float[] xrange, float[] yrange, float shiftx = .0f, float shifty = .0f, int direction=1)
     {
@@ -40,6 +40,44 @@ public class CameraAndGame : MonoBehaviour
         return Mathf.Abs(v1.x) <= Mathf.Abs(v2.x) && Mathf.Abs(v1.y) <= Mathf.Abs(v2.y) && Mathf.Abs(v1.z) <= Mathf.Abs(v2.z);
     }
 
+    int Preprocess(List<Vector3> p1, List<Vector3> p2)
+    {
+        for (int i = 0; i < p1.Count; i++)
+        {
+            for (int j = i; j > 0; j--)
+            {
+                if (smaller(p1[i] - p2[j], new Vector3(width, 1, width)))
+                {
+                    return j + 1;
+                }
+            }
+        }
+        for (int i = 0; i < p1.Count; i++)
+        {
+            for (int j = i; j > 0; j--)
+            {
+                if (smaller(p1[j] - p2[i], new Vector3(width, 1, width)))
+                {
+                    return j + 1;
+                }
+            }
+        }
+        return p1.Count - 1;
+    }
+
+    void ConnectPoints(Vector3 first,Vector3 last)
+    {
+        var alpha = -Mathf.Atan2(first.z - last.z, first.x - last.x);
+        var rotation = new Vector3(0, alpha * 180 / Mathf.PI, 0);
+        var barrier = Resources.Load<GameObject>("WallCube");
+        var loc = (last + first) / 2;
+        var size = new Vector3((last - first).magnitude, 1, width);
+        var obj = Instantiate(barrier, loc, Quaternion.Euler(rotation));
+        obj.transform.localScale = size;
+
+        last = transform.position;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -47,42 +85,23 @@ public class CameraAndGame : MonoBehaviour
         float[] xrange = { .0f, length * 1.5f}, yrange = { .0f, length * 1.5f };
         List<List<Vector3>> locs1 = create_map(xrange, yrange);
         List<List<Vector3>> locs2 = create_map(xrange, yrange, 15.0f, 15.0f, -1);
-        int blocked = 0;
-        for(int i = 0; i < locs1[0].Count; i++)
-        {
-            if(blocked % 2 == 0)
-            {
-                Instantiate(Resources.Load("WallCube"), locs1[0][i], Quaternion.identity);
-                Instantiate(Resources.Load("WallCube"), locs2[1][i], Quaternion.identity);
-                for(int j = i; j > 0; j--)
-                {
-                    if (smaller(locs1[0][i] - locs2[1][j], new Vector3(1, 1, 1))||
-                        smaller(locs1[0][j] - locs2[1][i], new Vector3(1, 1, 1)))
-                    {
-                        Debug.Log(locs2[0][i]);
-                        blocked += 1;
-                        break;
-                    }
-                }
-                
-            }
-           if (blocked < 2)
-            {
-                Instantiate(Resources.Load("WallCube"), locs1[1][i], Quaternion.identity);
-                Instantiate(Resources.Load("WallCube"), locs2[0][i], Quaternion.identity);
-                for (int j = i; j > 0; j--)
-                {  
-                        if (smaller(locs1[1][i] - locs2[0][j], new Vector3(1, 1, 1)) ||
-                            smaller(locs1[1][j] - locs2[0][i], new Vector3(1, 1, 1)))
-                        {
-                            Debug.Log(locs1[1][i]);
-                            blocked += 2;
-                            break;
-                        }
-                }
-            }
 
+        int l1 = Preprocess(locs1[0], locs2[1]);
+        int l2 = Preprocess(locs1[1], locs2[0]);
+
+        for(int i = 1; i <= l1; i++)
+        {
+            ConnectPoints(locs1[0][i - 1], locs1[0][i]);
+            ConnectPoints(locs2[1][i - 1], locs2[1][i]);
+            //Instantiate(Resources.Load("WallCube"), locs1[0][i], Quaternion.identity);
+            //Instantiate(Resources.Load("WallCube"), locs2[1][i], Quaternion.identity);
         }
+        for (int i = 1; i <= l2; i++)
+        {
+            Instantiate(Resources.Load("WallCube"), locs1[1][i], Quaternion.identity);
+            Instantiate(Resources.Load("WallCube"), locs2[0][i], Quaternion.identity);
+        }
+
 
     }
 
