f32d8656bd6ba3e9ca927a69673989e3c6d91a8d
Halotzi <liadhalotzi@gmail.com>
2022-01-28 16:31:58 +0200

    MaimMenu
    Added main menu to the game (volume settings not working yet)

---
+++ Assets/Prefabs/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPause = false;
+    public GameObject pauseMenuUI;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPause)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false); 
+        Time.timeScale = 1f; //Normal time
+        GameIsPause = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f; //To freeze the game
+        GameIsPause = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loadng menu...");
+        Time.timeScale = 1f; //Because i paused the game, the time is 0. I need to get it back to normal
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quiting Game");
+        Application.Quit();
+    }
+}
